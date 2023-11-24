841f9da57efd400c8d6b10de6cd7de2574915727
Zulnurain <muhammad.zulnural@ideabox.pk>
2023-11-24 18:15:34 +0500

    update

---
+++ storage/framework/views/4bcfd9e0aa3c247e7898b247f6833128.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Alpha</title>
+    </head>
+    <body>
+        <h1>This is the Alpha page</h1>
+        <a href="/beta">Next</a>
+    </body>
+</html>
+<?php /**PATH C:\Projects\phpunit-tests\resources\views/alpha.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/80d691eaf8f70099cd490a24d33def0a.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Beta</title>
+    </head>
+    <body>
+        <h1>This is the Beta page</h1>
+        <a href="/alpha">Previous</a>
+    </body>
+</html>
+<?php /**PATH C:\Projects\phpunit-tests\resources\views/beta.blade.php ENDPATH**/ ?>
